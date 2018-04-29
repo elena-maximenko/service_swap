9254850330857ba805e0c41486d1982c77789048
elena-maximenko <amely.honey@gmail.com>
2018-04-29 23:18:53 +0300

    Added nice output with own .css-file and bootstrap.

--- Desktop/labs/Diploma/service_swap/src/main/client/src/App.tsx
+++ Desktop/labs/Diploma/service_swap/src/main/client/src/App.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
+import './index.css';
+
 class App extends React.Component<{}, any>{
     constructor(props: any) {
         super(props);
@@ -29,12 +33,42 @@ class App extends React.Component<{}, any>{
     return (
         <div className="App">
             <div>
-                <h2>Users</h2>
+                <div className="container">
+                    <table className="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>First name</th>
+                            <th>Last name</th>
+                            <th>Email</th>
+                            <th/>
+                            <th/>
+                        </tr>
+                        </thead>
                 {users.map((user: any) =>
-                    <div key={user.id}>
-                        {user.firstName} {user.lastName}
-                    </div>
+                            <tr key={user.id}>
+                            <td>
+                                {user.firstName}
+                            </td>
+                            <td>
+                                {user.lastName}
+                            </td>
+                            <td>
+                                {user.login}
+                            </td>
+                            <td>
+                                <button className="glyphicon-button">
+                                    <i className="fas fa-pencil-alt"/>
+                                </button>
+                            </td>
+                            <td>
+                                <button className="glyphicon-button-delete" data-style="color: red">
+                                    <i className="fas fa-trash-alt"/>
+                                </button>
+                            </td>
+                            </tr>
                 )}
+                    </table>
+                </div>
             </div>
         </div>
     );
